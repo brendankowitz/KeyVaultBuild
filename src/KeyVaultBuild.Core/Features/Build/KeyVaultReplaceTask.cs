0e8db7f992d645df1668ab1ae67b74f9309bddb6
Brendan Kowitz <bkowitz@microsoft.com>
2017-04-04 14:50:52 -0700

    adding try/catch

--- src/KeyVaultBuild.Core/Features/Build/KeyVaultReplaceTask.cs
+++ src/KeyVaultBuild.Core/Features/Build/KeyVaultReplaceTask.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using KeyVaultBuild.Features.Transformation;
 using Microsoft.Build.Framework;
@@ -19,20 +21,42 @@ namespace KeyVaultBuild.Features.Build
 
         public string OverrideVaults { get; set; }
 
+        public string DebugTask { get; set; }
+
         public override bool Execute()
         {
-            var service = SecretServiceBuilder.Create()
-                .WithDirectory(DirectoryId)
-                .WithServicePrincipal(ClientId, Secret)
-                .Build();
-            var transformKey = new TransformKeys(service);
+            var debug = string.Equals("true", DebugTask, StringComparison.OrdinalIgnoreCase);
+            Config.Log.Information = x => Log.LogMessage(x);
+            Config.Log.Error = (ex, m) => Log.LogMessage("Error occured processing secrets. " + Environment.NewLine + ex);
 
-            var files = ConfigFiles.Select(file => file.GetMetadata("Fullpath")).ToArray();
-
-            Parallel.ForEach(files, file =>
+            try
             {
-                transformKey.SaveAsFile(file);
-            });
+                if (debug && !Debugger.IsAttached)
+                {
+                    Debugger.Launch();
+                    Debugger.Break();
+                }
+
+                var service = SecretServiceBuilder.Create()
+                    .WithDirectory(DirectoryId)
+                    .WithServicePrincipal(ClientId, Secret)
+                    .Build();
+                var transformKey = new TransformKeys(service);
+
+                var files = ConfigFiles.Select(file => file.GetMetadata("Fullpath")).ToArray();
+
+                Parallel.ForEach(files, file =>
+                {
+                    transformKey.SaveAsFile(file);
+                });
+            }
+            catch(Exception ex)
+            {
+                Config.Log.Error(ex, "Error occured processing secrets. ");
+                if (debug)
+                    Debugger.Break();
+                return false;
+            }
 
             return true;
         }
